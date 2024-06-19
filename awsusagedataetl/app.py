8e7c07d791c8da6d69878136e391957d35f0148b
“pravin <pravin@duplocloud.net>
2024-06-18 20:11:37 -0700

    etl initial

--- awsusagedataetl/app.py
+++ awsusagedataetl/app.py
@@ -5,127 +5,183 @@ import pandas as pd
 import boto3
 import sys
 import shutil
+from io import StringIO
+from datetime import datetime, timedelta
 
-class S3UsageDataFile:
+class AnalyticsS3File:
     def __init__(self, index, bucket_name, file_name, file_size, file_date, data_folder):
         self.index = index
         self.bucket_name = bucket_name
+        self.file_name = os.path.basename(file_name)
         self.s3_object_key = file_name
         self.file_size = file_size
         self.file_date = file_date
         self.local_file_name = data_folder + str(index) + "-" + os.path.basename(file_name)
         print(self.index, self.local_file_name, "size", file_size, "file_date", file_date, "bucket", bucket_name, "s3-file", self.s3_object_key)
 
-
-
-class AwsUsageDataReports:
+class DuploAwsAnalyticsEtl:
 
     def __init__(self):
+        self.event="usagedata"
         self.file_count = self.file_max_count = 10000000
         self.file_count = 0
-        self.out_folder = "/tmp/usagedata"
+        self.out_folder = "/tmp/" + self.event
         self.temp_out_folder = self.out_folder + "/reports_temp/"
-        self.usagedata_csv_folder = self.out_folder + "/reports_combined/"
+        self.analytics_event_csv_folder = self.out_folder + "/reports_combined/"
         self.data_folder = self.out_folder + "/data/"
-        self.dest_s3_bucket = "duplo-usagedata-reports"
-        self.start = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        self.start = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         self.s3_client = boto3.client('s3')
-        self.usagedata_report_files = []
+        self.analytics_event_report_files = []
         self.empty_buckets = []
 
-    def etl_on_customer_usagedata_s3_buckets(self, upload_s3_folder):
+    def etl_on_customer_analytics_event_s3_buckets(self, s3_dest_sub_folder):
+        # src s3
+        self.s3_src_file_prefix = "data/" + self.event + "/all"
+        self.s3_src_bucket_prefix = "duplo-analytics-"
+        # dest s3
+        self.s3_dest_sub_folder = s3_dest_sub_folder
+        self.dest_s3_bucket = "duplo-" + self.event + "-reports"
+        self.s3_dest_file_prefix = "data/" + s3_dest_sub_folder
+        self.s3_dest_reports_folder = "reports/"
+        self.cleanup_events_cutoff_date = datetime.now() - timedelta(days=3)
+
+        # log
         self._log(self.file_count, "temp_out_folder ", self.temp_out_folder)
-        self._log(self.file_count, "usagedata_csv_folder", self.temp_out_folder)
+        self._log(self.file_count, "analytics_event_csv_folder", self.temp_out_folder)
         self._log(self.file_count, "data_folder", self.data_folder)
+
+        # recreate_local_folder
         self.recreate_local_folder(self.temp_out_folder)
-        self.recreate_local_folder(self.usagedata_csv_folder)
+        self.recreate_local_folder(self.analytics_event_csv_folder)
         self.recreate_local_folder(self.data_folder)
 
-        #s3_file_prefix = "data/usagedata-data-12months/last-12-months/"
-        s3_file_prefix = "data/usagedata/all/"
-        s3_bucket_prefix = "duplo-analytics-"
+        # list_buckets
+        try:
+            print(f"start self._do_etls()")
+            #self._do_etls()
+            print(f"done self._do_etls()")
+        except Exception as e:
+            print(f"Error self._do_etls(): {str(e)}")
 
+        # save empty buckets
+        try:
+            print(f"start  self._save_analytics_event_report_csv()")
+            self._save_analytics_event_report_csv()
+            print(f"done  self._save_analytics_event_report_csv()")
+        except Exception as e:
+            print(f"Error  self._save_analytics_event_report_csv(): {str(e)}")
+
+        # save processed files
+        try:
+            print(f"start  self._save_analytics_event_report_csv()")
+            self._save_analytics_event_report_csv()
+            print( f"done  self._save_analytics_event_report_csv()")
+        except Exception as e:
+            print(f"Error  self._save_analytics_event_report_csv(): {str(e)}")
+
+        # Upload csvs
+        try:
+            print("start upload_s3_folder")
+            self._upload_csv_files()
+            print(f"done  self.upload_csv_files()")
+        except Exception as e:
+            print(f"Error self.upload_csv_files(): {str(e)}")
+
+        # Upload csvs
+        try:
+            print("start _cleanup_events()")
+            self._cleanup_events()
+            print(f"done self._cleanup_events()")
+        except Exception as e:
+            print(f"Error self._cleanup_events(): {str(e)}")
+
+    def _do_etls(self):
         response = self.s3_client.list_buckets()
-        buckets = [bucket['Name'] for bucket in response['Buckets'] if bucket['Name'].startswith(s3_bucket_prefix)]
-
+        buckets = [bucket['Name'] for bucket in response['Buckets'] if
+                   bucket['Name'].startswith(self.s3_src_bucket_prefix)]
         for bucket in buckets:
             paginator = self.s3_client.get_paginator('list_objects_v2')
             for page in paginator.paginate(Bucket=bucket):
-                self._log(self.file_count,"bucket", bucket)
-                if len(page.get('Contents', []))== 0:
+                self._log(self.file_count, "bucket", bucket)
+                if len(page.get('Contents', [])) == 0:
                     self.empty_buckets.append(bucket)
                 for obj in page.get('Contents', []):
                     file_name = obj['Key']
                     file_size = obj['Size']
                     file_date = obj['LastModified']
-                    if s3_file_prefix in file_name and "json" in file_name:
+                    if self.s3_src_file_prefix in file_name and "json" in file_name:
+                        print("found s3 event file_name ", file_name)
                         self.file_count += 1
-                        s3_usagedata_file = S3UsageDataFile(self.file_count, bucket, file_name, file_size, file_date, self.data_folder)
-                        self._process_s3_usagedata_file(s3_usagedata_file)
-                        self.usagedata_report_files.append(s3_usagedata_file)
+                        s3_analytics_event_file = AnalyticsS3File(self.file_count, bucket, file_name, file_size,
+                                                                  file_date, self.data_folder)
+                        self._process_s3_analytics_event_file(s3_analytics_event_file)
+                        self.analytics_event_report_files.append(s3_analytics_event_file)
 
-        # save empty buckets
-        self._save_empty_buckets_csv()
-        # save processed files
-        self._save_usagedata_report_csv()
-        # Upload csvs
-        self.upload_csv_files(upload_s3_folder)
-
-    def _process_s3_usagedata_file(self, s3_usagedata_file):
-        self.file_count += 1
-        self.file_count = s3_usagedata_file.file_index
-        dest_s3_object_key = upload_s3_folder + "/" + s3_usagedata_file.file_name
-        self._log(self.file_count,
-                  "_process_s3_usagedata_file " + s3_usagedata_file.bucket_name
-                  + " src: " + s3_usagedata_file.s3_object_key
+    def _process_s3_analytics_event_file(self, s3_analytics_event_file):
+        dest_s3_object_key = "data/" + self.s3_dest_sub_folder + "/" + s3_analytics_event_file.file_name
+        src_s3_object_key = s3_analytics_event_file.bucket_name + "/" + s3_analytics_event_file.s3_object_key
+        self._log(s3_analytics_event_file.index,
+                  "_process_s3_analytics_event_file "
+                  + " src: " + src_s3_object_key
                   + " dest: " + dest_s3_object_key)
-        copy_source = {
-            'Bucket': s3_usagedata_file.bucket_name,
-            'Key': s3_usagedata_file.s3_object_key
-        }
-        s3.meta.client.copy(copy_source, self.dest_s3_bucket, dest_s3_object_key)
-        s3.Object(s3_usagedata_file.bucket_name, s3_usagedata_file.s3_object_key).delete()
+        try:
+            copy_source = {
+                'Bucket': s3_analytics_event_file.bucket_name,
+                'Key': s3_analytics_event_file.s3_object_key
+            }
+            self.s3_client.copy_object(CopySource=src_s3_object_key, Bucket=self.dest_s3_bucket, Key=dest_s3_object_key)
+            print(f"File copied successfully from {s3_analytics_event_file.bucket_name}/{s3_analytics_event_file.s3_object_key} to {self.dest_s3_bucket}/{dest_s3_object_key}")
+        except Exception as e:
+            print(f"Error copying file: {str(e)}")
 
-    def upload_csv_files(self, upload_s3_folder):
-        for csv_file in os.listdir(self.usagedata_csv_folder):
-            if csv_file.endswith(".csv"):
-                file_path = self.usagedata_csv_folder + csv_file
-                file_stats = os.stat(file_path)
-                file_size = f"{round(file_stats.st_size / 1024)} kB"
-                self._log(self.file_count, "csv", file_path, "fileSize", file_size)
-                with open(file_path, "rb") as f:
-                    self.s3_client.upload_fileobj(f, self.dest_s3_bucket, upload_s3_folder + "/" + csv_file)
+        try:
+            #self.s3_client.delete_object(Bucket=s3_analytics_event_file.bucket_name, Key=s3_analytics_event_file.s3_object_key)
+            print(f"File copied successfully from {s3_analytics_event_file.bucket_name}/{s3_analytics_event_file.s3_object_key} to {self.dest_s3_bucket}/{dest_s3_object_key}")
+        except Exception as e:
+            print(f"Error deleting file: {str(e)}")
+
+    def _cleanup_events(self):
+        # list
+        response = self.s3_client.list_buckets(Bucket=self.dest_s3_bucket)
+        buckets = [bucket['Name'] for bucket in response['Buckets'] if bucket['Name'].startswith(self.s3_dest_file_prefix)]
+        for bucket in buckets:
+            paginator = self.s3_client.get_paginator('list_objects_v2')
+            for page in paginator.paginate(Bucket=bucket):
+                self._log(self.file_count, "bucket", bucket)
+                if len(page.get('Contents', [])) == 0:
+                    self.empty_buckets.append(bucket)
+                for obj in page.get('Contents', []):
+                    file_name = obj['Key']
+                    file_date = obj['LastModified']
+                    if file_date > self.cleanup_events_cutoff_date and "json" in file_name:
+                        print("found file_name ", file_name)
+                        try:
+                            #self.s3_client.delete_object(Bucket=self.dest_s3_bucket, Key=file_name)
+                            print(f"File copied successfully from {self.dest_s3_bucket}/{file_name}")
+                        except Exception as e:
+                            print(f"Error _cleanup_event {file_date.file_date.strftime('%Y-%m-%d %H:%M:%S')}"
+                                  f" file: {file_name} {str(e)}")
 
     def _save_empty_buckets_csv(self):
-        empty_buckets_file = self.usagedata_csv_folder + "empty_buckets.csv"
+        empty_buckets_file = self.analytics_event_csv_folder + "aws_events_analytics_empty_buckets.csv"
         empty_buckets_json = json.dumps([{
             "bucket": bucket
         } for bucket in self.empty_buckets])
-        sum_df = pd.read_json(empty_buckets_json)
+        sum_df = pd.read_json(StringIO(empty_buckets_json))
         sum_df.to_csv(empty_buckets_file, index=False, header=True)
 
-    def convert_size(self, bytes):
-        kb = bytes / 1024
-        mb = kb / 1024
-        if mb >= 1:
-            return f"{mb:.2f} MB"
-        elif kb >= 1:
-            return f"{kb:.2f} KB"
-        else:
-            return f"{bytes} bytes"
-    def _save_usagedata_report_csv(self):
-        usagedata_report_file = self.usagedata_csv_folder + "usagedata_report_files.csv"
-        usagedata_report_json = json.dumps([{
-            "file_index": s3_file.file_index,
-            "file_size": self.convert_size(s3_file.file_size),
-            "bucket_name": s3_file.bucket_name,
-            "s3_object_key": s3_file.s3_object_key,
-            "file_date": s3_file.file_date
-        } for s3_file in self.usagedata_report_files])
-        sum_df = pd.read_json(usagedata_report_json)
-        sum_df.to_csv(usagedata_report_file, index=False, header=True)
-
-
+    def _save_analytics_event_report_csv(self):
+        analytics_event_report_file = self.analytics_event_csv_folder + "aws_events_analytics_report_files.csv"
+        if len( self.analytics_event_report_files) > 0 :
+            analytics_event_report_json = json.dumps([{
+                "file_index": s3_file.index,
+                "file_size": self.convert_size(s3_file.file_size),
+                "bucket_name": s3_file.bucket_name,
+                "s3_object_key": s3_file.s3_object_key,
+                "file_date": s3_file.file_date.strftime('%Y-%m-%d %H:%M:%S')
+            } for s3_file in self.analytics_event_report_files])
+            sum_df = pd.read_json(StringIO(analytics_event_report_json))
+            sum_df.to_csv(analytics_event_report_file, index=False, header=True)
 
     def recreate_local_folder(self, folder_path):
         try:
@@ -148,16 +204,36 @@ class AwsUsageDataReports:
         except Exception as e:
             self._log(self.file_count, f"Error delete occurred: {e} " + file_path)
 
+    def _upload_csv_files(self):
+        for csv_file in os.listdir(self.analytics_event_csv_folder):
+            if csv_file.endswith(".csv"):
+                file_path = self.analytics_event_csv_folder + csv_file
+                file_stats = os.stat(file_path)
+                file_size = f"{round(file_stats.st_size / 1024)} kB"
+                self._log(self.file_count, "csv", file_path, "fileSize", file_size)
+                with open(file_path, "rb") as f:
+                    self.s3_client.upload_fileobj(f, self.dest_s3_bucket, self.s3_dest_reports_folder + csv_file)
+
+    def convert_size(self, bytes):
+        kb = bytes / 1024
+        mb = kb / 1024
+        if mb >= 1:
+            return f"{mb:.2f} MB"
+        elif kb >= 1:
+            return f"{kb:.2f} KB"
+        else:
+            return f"{bytes} bytes"
+
     def _log(self, message, *args, **kwargs):
         # timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         # print(f"{timestamp} - {message}", *args, **kwargs)
         print(f"{message}", *args, **kwargs)
 
 def handler(event, context):
-    aws_usagedata_reports = AwsUsageDataReports()
-    aws_usagedata_reports.etl_on_customer_usagedata_s3_buckets("aws")
-    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    return f"total reports: {str(aws_usagedata_reports.file_count)}, start: {aws_usagedata_reports.start}, end: {timestamp}, v:{sys.version} !"
+    aws_analytics_event_reports = DuploAwsAnalyticsEtl()
+    aws_analytics_event_reports.etl_on_customer_analytics_event_s3_buckets("aws")
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return f"total reports: {str(aws_analytics_event_reports.file_count)}, start: {aws_analytics_event_reports.start}, end: {timestamp}, v:{sys.version} !"
 
 
-#handler(None, None)
+handler(None, None)
--- oci_spark_deployments/spark-job/tf/scripts/_ope_airflow.sh
+++ oci_spark_deployments/spark-job/tf/scripts/_ope_airflow.sh
@@ -19,8 +19,8 @@ open $airflow
 aws mwaa create-web-login-token --name  duploservices-demo01-airflow1 --profile duplo-jsr --query '{tkn:WebToken,host:WebServerHostname}'
 
 'Volumes[*]'
-export duplo_token="AQAAANCMnd8BFdERjHoAwE_Cl-sBAAAAQk4I8I2z4E-VPI4m6Vb5hgAAAAACAAAAAAAQZgAAAAEAACAAAABY1Kiak9zMi4whTa4KK3AmTY6d7H2XmaWPgZT4ve7PXwAAAAAOgAAAAAIAACAAAABiA0wg-06YaRAGB1oKDH27z6F2kl5fwUmiQTIwPaUzFKAAAADdSu0BtjoM9L0KPv-6_JGq-GLGkafDsm1OdhtzyyLkO_XvhiGeSZaqj35qju-5hgRf2W6KAvw27ZoZB1ZjRz56XULgn17-WZMUKlXvuT7urCX8pO_ALetU5EIVoYbJm6SkIEQdBDlqgGXsVIuh338tyhN-SrUVCC7M1vYnHAmDVjzyydEm8UpmwsYvJt93szWAhxdSRIyZbfTzjAWNy4v-QAAAAOd3IihzEBHFlXiCW5C25PD1zwiYXg0OERZcTAG8z7KUhBd-7sR2Th_bn2ze3oyTagdSklreGNQkk7CO9tXcFzY"
-export duplo_host="https://ia.duplocloud.net"
+export duplo_token="xxx"
+export duplo_host="https://xxx.duplocloud.net"
 export tokenpath="adminproxy/GetJITAwsConsoleAccessUrl"
 
 curl -Ssf -H 'Content-type: application/json' \
